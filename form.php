dfc4df8c2aae94818667f8386adb099535b3fda4
Syn <suryanugraha355@gmail.com>
2024-10-30 16:22:56 +0700

    First Commit

---
+++ form.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Latihan Form</title>
+</head>
+<body>
+    <form action="/controller.php" method=get>
+        <label for="username">Username</label> <br>
+        <input type="text" name="uname"> <br>
+        <label for="password">Password</label> <br>
+        <input type="password" name="pass"> <br> <br>
+        <label for="email">Email</label> <br>
+        <input type="email" name="email"> <br> <br>
+        <input type="submit" value="kirim">
+    </form>
+</body>
+</html> 
